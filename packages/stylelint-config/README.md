f6946429b20ec9ea4176d9e7403e2059995b9853
Dominik Schilling <dominikschilling+git@gmail.com>
2020-05-22 10:38:17 +0200

    Add stylelint-config

---
+++ packages/stylelint-config/README.md
@@ -0,0 +1,21 @@
+# required (S)CSS Coding Standard
+
+required coding standard for (S)CSS, based on the rules provided by [`stylelint-config-wordpress`](https://github.com/WordPress-Coding-Standards/stylelint-config-wordpress).
+
+## Installation
+
+This package is an ESLint shareable configuration and requires `stylelint` and `stylelint-config-wordpress` to be installed. To install this config and the peerDependencies run:
+
+```bash
+npx install-peerdeps --dev @wearerequired/stylelint-config@latest
+```
+
+## Usage
+
+To opt-in to the default configuration, extend your own project's `.stylelintrc` file:
+
+```json
+{
+	"extends": [ "@wearerequired/stylelint-config" ]
+}
+```
---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,41 @@
+module.exports = {
+	extends: 'stylelint-config-wordpress',
+	rules: {
+		'max-line-length': 100,
+		'max-empty-lines': 1,
+		'rule-empty-line-before': [
+			'always',
+			{
+				except: [ 'first-nested' ],
+				ignore: [ 'after-comment' ],
+			},
+		],
+		'comment-empty-line-before': [
+			'always',
+			{
+				except: [ 'first-nested' ],
+				ignore: [ 'stylelint-commands' ],
+			},
+		],
+		'at-rule-empty-line-before': [
+			'always',
+			{
+				except: [ 'blockless-after-blockless', 'first-nested' ],
+				ignore: [ 'after-comment' ],
+			},
+		],
+		'media-feature-parentheses-space-inside': 'always',
+		'function-parentheses-space-inside': 'always-single-line',
+		'function-comma-space-after': 'always-single-line',
+		'number-leading-zero': 'never',
+		'selector-class-pattern': [
+			// See https://en.bem.info/methodology/naming-convention/#two-dashes-style.
+			'^(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$',
+			{
+				resolveNestedSelectors: true,
+				message:
+					"Selector should use lowercase and hyphens to separate words or in BEM style: block-name__elem-name--mod-name (selector-class-pattern)'",
+			},
+		],
+	},
+};
